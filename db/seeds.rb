39d5e02a67150ce72055d798672fde5273cff15d
Subhan-khaliq <subhankhaiq420@gmail.com>
2022-08-19 20:40:09 +0500

    New updation

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Article.destroy_all
+
+Article.create!([{
+  title: "Soul",
+  body: "Pete Docter",
+},
+{
+  title: "The Lord of the Rings: The Fellowship of the Ring",
+  body: "Peter Jackson",
+
+},
+{
+  title: "Terminator 2",
+  body: "James Cameron",
+
+}])
+
+p "Created #{Article.count} articles"
